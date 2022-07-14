994a3e3153fcdec7ea5026977e0075eb610d4ee3
J0YERIM <96174711+J0YERIM@users.noreply.github.com>
2022-07-14 18:28:40 +0900

    Update settings.py

--- devsns/devsns/settings.py
+++ devsns/devsns/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a#a0mi*65rjg9qss1%4fkj=$-24e4v%q_2igx=9-@mkbrimpws'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
